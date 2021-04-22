2afa20076ef20f0d101339d7a47db6486fac7991
Niko Nieminen <niko.m.nieminen@helsinki.fi>
2021-04-22 14:38:07 +0300

    Starter website refactoring

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -1,4 +1,4 @@
-"""project1 URL Configuration
+"""mysite URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.1/topics/http/urls/
@@ -14,8 +14,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 
 urlpatterns = [
+    path('myapp/', include('myapp.urls')),
     path('admin/', admin.site.urls),
 ]
