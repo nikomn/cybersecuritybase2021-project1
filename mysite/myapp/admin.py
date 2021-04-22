7c4fb09e2146361d57afdbb3044aa78fae66d267
Niko Nieminen <niko.m.nieminen@helsinki.fi>
2021-04-22 15:42:58 +0300

    Early attempt at creating part of OWASP #2

--- mysite/myapp/admin.py
+++ mysite/myapp/admin.py
@@ -3,5 +3,7 @@ from django.contrib import admin
 # Register your models here.
 
 from .models import Account
+from .models import User
 
 admin.site.register(Account)
+admin.site.register(User)
--- mysite/myapp/models.py
+++ mysite/myapp/models.py
@@ -1,7 +1,11 @@
 from django.db import models
-from django.contrib.auth.models import User
+#from django.contrib.auth.models import User
 
 # Create your models here.
+
+class User(models.Model):
+  username = models.TextField()
+  password = models.TextField()
 
 class Account(models.Model):
   owner = models.ForeignKey(User, on_delete=models.CASCADE)
