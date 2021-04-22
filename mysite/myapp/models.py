fcb0a2b910c2bbb2ca090af00db963db8e4f84d5
Niko Nieminen <niko.m.nieminen@helsinki.fi>
2021-04-22 15:18:12 +0300

    Initial setup tasks

--- mysite/myapp/models.py
+++ mysite/myapp/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Account(models.Model):
+  owner = models.ForeignKey(User, on_delete=models.CASCADE)
+  uid = models.TextField()
+  def __str__(self):
+    return self.uid
+
--- mysite/myapp/views.py
+++ mysite/myapp/views.py
@@ -1,7 +1,15 @@
-from django.shortcuts import render
 from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from .models import Account
+from django.db.models import Q
+import json
+
 
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the myapp index.")
+    return render(request, 'pages/index.html')
+    # return render(request, 'pages/index.html')
+    # return HttpResponse("Hello, world. You're at the myapp index.")
